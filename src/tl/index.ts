f610e1df32ba08b7ddb12a1c6a52b2f5c290ad84
Mikhail Isupov <lam0x86@gmail.com>
2020-04-13 00:39:30 +0300

    Minor eslint fixes

--- src/tl/index.ts
+++ src/tl/index.ts
@@ -3,18 +3,14 @@ import { default as parseMtproto } from './mtproto/parser';
 import { default as buildLayer105 } from './layer105/builder';
 import { default as parseLayer105 } from './layer105/parser';
 import { Reader32, Writer32 } from '../serialization';
+import { MethodDeclMap as MtprotoMethodDeclMap } from './mtproto/types';
+import { MethodDeclMap as Layer105MethodDeclMap } from './layer105/types';
 
 // types
 export {
   ResPQ, BadMsgNotification, NewSession, RpcResult, Req_DH_params,
-  Set_client_DH_params, Server_DH_inner_data, 
+  Set_client_DH_params, Server_DH_inner_data,
 } from './mtproto/types';
-
-import { MethodDeclMap as MtprotoMethodDeclMap } from './mtproto/types';
-import { MethodDeclMap as Layer105MethodDeclMap } from './layer105/types';
-
-export interface MethodDeclMap extends MtprotoMethodDeclMap, Layer105MethodDeclMap {
-}
 
 export {
   UpdateDeclMap,
@@ -22,13 +18,16 @@ export {
   InputCheckPasswordSRP,
 } from './layer105/types';
 
+export interface MethodDeclMap extends MtprotoMethodDeclMap, Layer105MethodDeclMap {
+}
+
 export const parse = (reader: Reader32) => parseMtproto(reader, parseLayer105);
 
-const sharedBuffer = new Uint32Array(1024);
-const w = new Writer32(sharedBuffer);
-  
+const sharedBuffer = new Uint32Array(128 * 1024);
+const writer = new Writer32(sharedBuffer);
+
 export const build = (o: any) => {
-  w.pos = 0;
-  buildMtproto(w, o, () => buildLayer105(w, o));
-  return w.buf.subarray(0, w.pos);
+  writer.pos = 0;
+  buildMtproto(writer, o, () => buildLayer105(writer, o));
+  return writer.buf.subarray(0, writer.pos);
 };
